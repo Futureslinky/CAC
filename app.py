bad17d57f7d03d3ad009e1b71ed3c8c021ac0b08
Harsh <77411358+hershey-harsh@users.noreply.github.com>
2023-07-11 09:47:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,5 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+        app.run(host="0.0.0.0")
